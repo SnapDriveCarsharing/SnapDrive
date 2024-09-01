230570cdeff3a712b4e340e350550082c9f81618
Vadym <Servicepost.v@gmail.com>
2024-09-01 18:07:24 +0200

    initial commit

---
+++ src/main/java/com/snapdrive/snapdrive/SnapDriveApplication.java
@@ -0,0 +1,13 @@
+package com.snapdrive.snapdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapDriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snapdrive/snapdrive/SnapDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapdrive.snapdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
